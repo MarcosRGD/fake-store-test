1f67b56f8e5e8ca656447f2b7cdd542ee670b0ea
Tr1sk33l <mastermind51@gmail.com>
2022-04-25 16:03:59 -0300

    banner
    banner component

---
+++ src/component/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="hero">
+      <div class="card bg-dark text-white border-0">
+        <img
+          class="card-img"
+          src="/assets/banner.jpg"
+          alt="banner"
+          height="550px"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
